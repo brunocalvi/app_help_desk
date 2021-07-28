d3c6a9ce38242d0d723ce28b8d1e344fc1ce25d4
brunocalvi <calvireis@gmail.com>
2021-07-28 20:18:01 -0300

    Finalizando o projeto

--- valida_login.php
+++ valida_login.php
@@ -1,19 +1,20 @@
 <?php
 
 session_start();
-//$_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
-//print_r($_SESSION);
-//echo '<hr/>';
-//print_r($_SESSION['y']);
-
 
 //VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
 $usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
+
+$perfis = array(1 => 'Administrativo', 2 => 'Usuário');
 
 //USUARIOS DO SISTEMA
 $usuarios_app = array(
-    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => 'adm', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => 'user', 'perfil_id' => 1),
+    array('id' => 3, 'email' => 'joao@teste.com.br', 'senha' => 'joao', 'perfil_id' => 2),
+    array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => 'maria', 'perfil_id' => 2),
 );
 /*
 
@@ -24,14 +25,12 @@ echo '</pre>';
 */
 
 foreach($usuarios_app as $user){
-    /*
-    echo 'Usuario app: ' . $user['email'] . '/' . $user['senha'];
-    echo '<br />';
-    echo 'Usuario form: ' . $_POST['email'] . '/' . $_POST['senha'];
-    echo '<hr />';
-    */
+ 
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
+
     }
 }
 
@@ -39,8 +38,8 @@ foreach($usuarios_app as $user){
         echo 'Usuário autenticado.';
 
         $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['X'] = 'Um Valor';
-        $_SESSION['Y'] = 'Outro valor';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id; 
         header('Location: home.php');
 
     }else{  
